2767ff4ce2f7dab132e631c763537bac22d314d7
Xvirus0 <131556346+Xvirus0@users.noreply.github.com>
2023-04-23 20:50:31 +0200

    Fixing update

--- util/plugins/update.py
+++ util/plugins/update.py
@@ -24,7 +24,7 @@ def search_for_updates():
     result_string = soup[s1.end() : s2.start()]
 
     if THIS_VERSION not in result_string:
-        setTitle("Hazard Nuker New Update Found!")
+        setTitle("Xvirus New Update Found!")
         print(
             f"""{Fore.YELLOW}
                 ███╗   ██╗███████╗██╗    ██╗    ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗██╗
@@ -33,14 +33,14 @@ def search_for_updates():
                 ██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  ╚═╝
                 ██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗██╗
                 ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝
-                              {Fore.RED}Looks like this Hazard Nuker {THIS_VERSION} is outdated """.replace(
+                              {Fore.RED}Looks like this Xvirus {THIS_VERSION} is outdated """.replace(
                 "█", f"{Fore.WHITE}█{Fore.RED}"
             ),
             end="\n\n",
         )
         soup = BeautifulSoup(
             requests.get(
-                "https://github.com/KDot227/hazard-nuker-mirror/releases"
+                "https://github.com/Xvirus0/Xvirus-Tools/releases"
             ).text,
             "html.parser",
         )
@@ -48,48 +48,48 @@ def search_for_updates():
             if "releases/download" in str(link):
                 update_url = f"https://github.com/{link.get('href')}"
         choice = input(
-            f"{Fore.GREEN}[{Fore.YELLOW}>>>{Fore.GREEN}] {Fore.RESET}You want to update to the latest version? (Y to update): {Fore.RED}"
+            f"{Fore.GREEN}[{Fore.YELLOW}>>>{Fore.GREEN}] {Fore.RESET}Do you want to update to the latest version? (Y to update N to continue): {Fore.RED}"
         )
 
         if choice.lower() == "y" or choice.lower() == "yes":
             print(f"{Fore.WHITE}\nUpdating. . .")
-            setTitle(f"Hazard Nuker Updating...")
-            # if they are running hazard.exe
+            setTitle(f"Xvirus Updating...")
+            # if they are running xvirus.exe
             if os.path.basename(sys.argv[0]).endswith("exe"):
-                with open("HazardNuker.zip", "wb") as zipfile:
+                with open("Xvirus-Tools.zip", "wb") as zipfile:
                     zipfile.write(requests.get(update_url).content)
-                with ZipFile("HazardNuker.zip", "r") as filezip:
+                with ZipFile("Xvirus-Tools.zip", "r") as filezip:
                     filezip.extractall()
-                os.remove("HazardNuker.zip")
-                cwd = os.getcwd() + "\\HazardNuker\\"
+                os.remove("Xvirus-Tools.zip")
+                cwd = os.getcwd() + "\\Xvirus-Tools\\"
                 shutil.copyfile(cwd + "Changelog.md", "Changelog.md")
                 try:
                     shutil.copyfile(
-                        cwd + os.path.basename(sys.argv[0]), "HazardNuker.exe"
+                        cwd + os.path.basename(sys.argv[0]), "Xvirus-Tools.exe"
                     )
                 except Exception:
                     pass
                 shutil.copyfile(cwd + "README.md", "README.md")
-                shutil.rmtree("HazardNuker")
-                setTitle("Hazard Nuker Update Complete!")
+                shutil.rmtree("Xvirus-Tools")
+                setTitle("Xvirus Update Complete!")
                 print(f"{Fore.GREEN}Update Successfully Finished!")
                 sleep(2)
-                os.startfile("HazardNuker.exe")
+                os.startfile("Xvirus-Tools.exe")
                 os._exit(0)
-            # if they are running hazard source code
+            # if they are running xvirus source code
             else:
                 new_version_source = requests.get(
-                    "https://github.com/KDot227/hazard-nuker-mirror/archive/refs/heads/main.zip"
+                    "https://github.com/Xvirus0/Xvirus-Tools/archive/refs/heads/main.zip"
                 )
-                with open("Hazard-Nuker-master.zip", "wb") as zipfile:
+                with open("Xvirus-Tools-master.zip", "wb") as zipfile:
                     zipfile.write(new_version_source.content)
-                with ZipFile("Hazard-Nuker-master.zip", "r") as filezip:
+                with ZipFile("Xvirus-Tools-master.zip", "r") as filezip:
                     filezip.extractall()
-                os.remove("Hazard-Nuker-master.zip")
-                cwd = os.getcwd() + "\\Hazard-Nuker-master"
+                os.remove("Xvirus-Tools-master.zip")
+                cwd = os.getcwd() + "\\Xvirus-Tools-master"
                 shutil.copytree(cwd, os.getcwd(), dirs_exist_ok=True)
                 shutil.rmtree(cwd)
-                setTitle("Hazard Nuker Update Complete!")
+                setTitle("Xvirus Update Complete!")
                 print(f"{Fore.GREEN}Update Successfully Finished!")
                 sleep(2)
                 if os.path.exists(os.getcwd() + "setup.bat"):
