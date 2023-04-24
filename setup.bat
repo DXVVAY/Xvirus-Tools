b0d11c432928ffd39f9de9c7a77441f9cdf54800
DEXV <89728480+DXVVAY@users.noreply.github.com>
2023-04-24 12:34:15 +0200

    remove some packages to add the in requiements

--- setup.bat
+++ setup.bat
@@ -3,9 +3,7 @@
 cd /d %~dp0
 
 pip install -U -r requirements.txt
-pip install cairocffi
 pipwin install cairocffi
-pip install cairosvg
 
 echo @echo off > start.bat
 echo python -m Xvirus >> start.bat
