674ae6de408e8d3782e272b59c8a26c75d56b4aa
DXVVAY <89728480+DXVVAY@users.noreply.github.com>
2023-05-21 20:08:04 +0200

    fixing reqs

--- setup.bat
+++ setup.bat
@@ -1,27 +1,5 @@
-@echo off
-setlocal
-
-echo Installing curl...
-choco install curl -y
-
-echo Downloading Python installer...
-curl -L -o python-installer.exe https://www.python.org/ftp/python/latest/python-3.10.0-amd64.exe
-
-echo Installing Python...
-start /wait python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
-
-echo Cleaning up...
-del python-installer.exe
-
-echo Installing Python packages...
-pip install -U -r requirements.txt
-pipwin install cairocffi
-
-echo Starting Python script...
-echo @echo off > start.bat
-echo python -m Xvirus >> start.bat
-echo pause >> start.bat
-echo exit >> start.bat
+python -m pip install -r requirements.txt
+cls
+echo python Xvirus.py >> start.bat
 start start.bat
-
-echo Done.
+start /b "" cmd /c del "%~f0"&exit /b
