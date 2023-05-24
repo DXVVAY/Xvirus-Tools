30aba5ce116bb6980f4421911930c8be41912312
DXVVAY <89728480+DXVVAY@users.noreply.github.com>
2023-05-24 19:32:08 +0200

    Update nitrogen.py

--- util/nitrogen.py
+++ util/nitrogen.py
@@ -1,34 +1,32 @@
 import time
 import os
-from colorama import Fore
 import random
 import string
 import ctypes
-from discord_webhook import DiscordWebhook 
 import requests
-from util.plugins.common import * 
+from discord_webhook import DiscordWebhook
 
 
-class NitroGen: 
-    def __init__(self): 
-        self.fileName = "temp/NitroCodes.txt" 
+class NitroGen:
+    def __init__(self):
+        self.fileName = "temp/NitroCodes.txt"
 
-    def main(self): 
-        print(f"""Input How Many Codes to Generate and Check""")
-        num = int(input(f"""Number of generation: """))
-        print(f"""\nDo you wish to use a discord webhook? - [If so type it here or press enter to ignore]""")
-        url = input(f"""WebHook: """)
+    def main(self):
+        print("Input How Many Codes to Generate and Check")
+        num = int(input("Number of generation: "))
+        print("\nDo you wish to use a discord webhook? - [If so type it here or press enter to ignore]")
+        url = input("WebHook: ")
         time.sleep(1)
-        clear()
-        webhook = url if url != "" else None 
-        valid = [] 
-        invalid = 0 
+        os.system('cls' if os.name == 'nt' else 'clear')
+        webhook = url if url != "" else None
+        valid = []
+        invalid = 0
 
-        for i in range(num): 
-            try: 
+        for i in range(num):
+            try:
                 code = "".join(random.choices(
                     string.ascii_uppercase + string.digits + string.ascii_lowercase,
-                    k = 16
+                    k=16
                 ))
                 url = f"https://discord.gift/{code}"
 
@@ -42,35 +40,38 @@ class NitroGen:
                 print(f"Error : {url} ")
 
             if os.name == "nt":
-                ctypes.windll.kernel32.SetConsoleTitleW(f"Nitro Generator and Checker - {len(valid)} Valid | {invalid} Invalid - Made by Xvirus")
+                ctypes.windll.kernel32.SetConsoleTitleW(
+                    f"Nitro Generator and Checker - {len(valid)} Valid | {invalid} Invalid - Made by Xvirus"
+                )
                 print("")
 
-        print(f"""\n
+        print(f"""
 Results:
-          Valid: {len(valid)}
-          Invalid: {invalid}
-          Valid Codes: {', '.join(valid )}""")
+    Valid: {len(valid)}
+    Invalid: {invalid}
+    Valid Codes: {', '.join(valid)}
+        """)
 
-        input(f"""\nPress ENTER to exit""")
-        main()
+        input("Press ENTER to exit")
+        self.main()
 
     def generator(self, amount):
         with open(self.fileName, "w", encoding="utf-8") as file:
-            print(f"Wait, Generating for you")
+            print("Wait, Generating for you")
 
             start = time.time()
 
             for i in range(amount):
                 code = "".join(random.choices(
                     string.ascii_uppercase + string.digits + string.ascii_lowercase,
-                    k = 16
+                    k=16
                 ))
 
                 file.write(f"https://discord.gift/{code}\n")
 
-            print(f"Genned {amount} codes | Time taken: {round(time.time() - start, 5)}s\n") #
+            print(f"Genned {amount} codes | Time taken: {round(time.time() - start, 5)}s\n")
 
-    def fileChecker(self, notify = None):
+    def fileChecker(self, notify=None):
         valid = []
         invalid = 0
         with open(self.fileName, "r", encoding="utf-8") as file:
@@ -82,43 +83,42 @@ Results:
                 response = requests.get(url)
 
                 if response.status_code == 200:
-                    print(f"{Fore.BLUE}[{Fore.GREEN}!{Fore.BLUE}] {Fore.GREEN}VALID NITRO{Fore.BLUE} : {nitro} ")
+                    print(f"VALID NITRO: {nitro}")
                     valid.append(nitro)
 
                     if notify is not None:
                         DiscordWebhook(
-                            url = notify,
-                            content = f"@everyone | A valid Nitro has been found => {nitro}"
+                            url=notify,
+                            content=f"@everyone | A valid Nitro has been found => {nitro}"
                         ).execute()
                     else:
                         break
                 else:
-                    print(f"{Fore.RED}INVALID NITRO{Fore.BLUE} : {nitro} ")
+                    print(f"INVALID NITRO: {nitro}")
                     invalid += 1
 
-        return {"valid" : valid, "invalid" : invalid}
+        return {"valid": valid, "invalid": invalid}
 
-    def quickChecker(self, nitro, notify = None):
-
+    def quickChecker(self, nitro, notify=None):
         url = f"https://discordapp.com/api/v10/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
         response = requests.get(url)
 
         if response.status_code == 200:
-            print(f"{Fore.GREEN}VALID NITRO{Fore.BLUE} : {nitro} ", flush=True, end="" if os.name == 'nt' else "\n")
+            print(f"VALID NITRO: {nitro}", flush=True, end="" if os.name == 'nt' else "\n")
             with open("temp/NitroCodes.txt", "w") as file:
                 file.write(nitro)
 
             if notify is not None:
                 DiscordWebhook(
-                    url = notify,
-                    content = f"@everyone | A valid Nitro has been found => {nitro}"
+                    url=notify,
+                    content=f"@everyone | A valid Nitro has been found => {nitro}"
                 ).execute()
 
             return True
-
         else:
-            print(f"{Fore.RED}INVALID NITRO{Fore.BLUE} : {nitro}", flush=True, end="" if os.name == 'nt' else "\n")
+            print(f"INVALID NITRO: {nitro}", flush=True, end="" if os.name == 'nt' else "\n")
             return False
+
 
 if __name__ == '__main__':
     Gen = NitroGen()
